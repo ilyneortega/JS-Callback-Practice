71228b1e3294be9cc73935a847c9494a6310f9f3
Ilyne Ortega <ilyneortega@ilynes-MBP.attlocal.net>
2023-01-29 00:24:01 -0800

    completed assignment

--- index.js
+++ index.js
@@ -2,10 +2,53 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+function handleDirectionChange(direction) {
+    if(direction === null) {
+     character.src = 'assets/green-character/static.gif'
+    }
+    if (direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }  
+    if (directioin === 'north') {
+        character.src === 'assets/green-character/north.gif'
+    }
+    if (direction === 'east') {
+        character.src = 'assets/green-character/east.gif'
+    }
+    if (direction === 'south') {
+        character.src = 'assets/green-character/south.gif'
+    }
+
+}
+ 
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+document.addEventListener('keydown', function(e) {        
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+
+})
 
 
-move(newImage('assets/tree.png')).to(200, 450)
+document.addEventListener('keyup', function(e) {
+    direction = null
+})
+
+
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
@@ -13,3 +56,5 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
